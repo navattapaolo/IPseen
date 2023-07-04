d6825a3fb1d0a5eca0ba8a998d565a57b7993c1e
Paolo Navatta <109438860+navattapaolo@users.noreply.github.com>
2023-07-04 13:13:20 +0200

    Update IPseen.bat

--- Windows/IPseen.bat
+++ Windows/IPseen.bat
@@ -1,12 +1,25 @@
 @echo off
+
+REM Ottieni il nome della rete corrente
+for /f "tokens=2 delims=: " %%i in ('netsh WLAN show interfaces ^| findstr /r /c:"^    SSID"') do (
+    set "networkName=%%i"
+    goto :done
+)
+:done
+
+REM Ottieni l'indirizzo IP pubblico
+for /f "delims=" %%i in ('curl -s https://ifconfig.me') do set "publicIP=%%i"
+
 if "%1"=="-mac" (
     ipconfig /all | findstr "Physical Address"
 ) else if "%1"=="-private" (
     ipconfig | findstr IPv4
 ) else if "%1"=="-public" (
-    curl -s https://ifconfig.me
+    echo Public IP Address:
+    echo    %networkName%...........: %publicIP%
 ) else (
     ipconfig /all
-	echo Public IP Address:
-	curl -s https://ifconfig.me
+    echo.
+    echo Public IP Address:
+    echo    %networkName%...........: %publicIP%
 )
