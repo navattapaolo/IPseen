d351b21a1aac2bcb1358cc28b58568e8f73aebaa
Paolo Navatta <109438860+navattapaolo@users.noreply.github.com>
2023-08-20 15:09:52 +0200

    Update IPseen.bat

--- Windows/IPseen.bat
+++ Windows/IPseen.bat
@@ -11,7 +11,7 @@ for /f "tokens=2 delims=: " %%i in ('netsh WLAN show interfaces ^| findstr /r /c
 REM Get the Public IP
 for /f "delims=" %%i in ('curl -s https://ifconfig.me') do set "publicIP=%%i"
 
-if "%1"=="-mac" (
+if "%1"=="--mac" (
     ipconfig /all | findstr "Physical Address"
 ) else if "%1"=="--private" (
     ipconfig | findstr IPv4
