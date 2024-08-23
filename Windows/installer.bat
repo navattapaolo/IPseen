3485b347f9f8f2490853d90e435b31c8c1ef4644
Paolo Navatta <109438860+navattapaolo@users.noreply.github.com>
2024-08-23 16:06:30 +0200

    Update installer.bat

--- Windows/installer.bat
+++ Windows/installer.bat
@@ -1,16 +1,31 @@
 @echo off
 setlocal
 
-REM Directory
-set "destination=%Path%"
+REM Define the directory where the script is located
+set "scriptDir=%~dp0"
 
-REM Current script name (assuming script extension is .bat)
-for %%F in ("%~f0") do set "script_name=%%~nxF"
+REM Define the name of the folder and environment variable
+set "folderName=IPseen"
+set "envVarName=IPseen"
 
-REM Copy files to the destination directory
-echo Copying files to %destination%...
-for %%I in (*) do (
-    if "%%~nI" neq "%script_name%" (
-        xcopy "%%~fI" "%destination%" /S /Y
-    )
+REM Define the full path for the folder
+set "folderPath=%scriptDir%%folderName%"
+
+REM Create the folder if it does not exist
+if not exist "%folderPath%" (
+    echo Creating folder "%folderPath%"
+    mkdir "%folderPath%"
+) else (
+    echo Folder "%folderPath%" already exists.
 )
+
+REM Set the user environment variable
+echo Setting user environment variable "%envVarName%" to "%folderPath%"...
+setx %envVarName% "%folderPath%"
+
+REM Notify the user
+echo User environment variable "%envVarName%" has been set to "%folderPath%".
+echo You can now use "%envVarName%" in your command prompts to reference the folder.
+
+REM Wait for a key press to close the prompt
+pause
